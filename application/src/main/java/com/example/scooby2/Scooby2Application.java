06ad220e7d841b06f2c0743777d73c4c825870e8
François <francois.lagoueyte@asso-cocktail.fr>
2017-10-02 10:59:11 +0200

    first commit

---
+++ application/src/main/java/com/example/scooby2/Scooby2Application.java
@@ -0,0 +1,12 @@
+package com.example.scooby2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scooby2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scooby2Application.class, args);
+	}
+}
